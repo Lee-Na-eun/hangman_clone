4defff0922a911f9ac6b4b0f56b1938c7c5fbfe0
Lee Naeun <nanni4287@gmail.com>
2022-03-08 22:03:49 +0900

    typescript 시작

--- ts/hangman.ts
+++ ts/hangman.ts
@@ -1,1 +1,30 @@
-console.log('hi');
+const programmingLanguages: Array<string> = [
+  'javascript',
+  'python',
+  'typescript',
+  'java',
+  'json',
+  'mongodb',
+  'php',
+  'sql',
+  'ruby',
+  'c',
+  'html',
+  'css',
+];
+
+let answer: string = '';
+let maxWrong: number = 6;
+let mistake: number = 0;
+let guessed: Array<string> = [];
+let wordStatus = null;
+
+const randomWord = () => {
+  answer =
+    programmingLanguages[
+      Math.floor(Math.random() * programmingLanguages.length)
+    ];
+  console.log(answer);
+};
+
+randomWord();
