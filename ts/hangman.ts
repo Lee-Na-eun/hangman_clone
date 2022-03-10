26ddd157bedfeb28d6b8a6128f5b82eebf2be8a8
Lee Naeun <nanni4287@gmail.com>
2022-03-10 16:00:36 +0900

    버튼 클릭시 맞는지 틀린지 확인

--- ts/hangman.ts
+++ ts/hangman.ts
@@ -35,7 +35,7 @@ const generateButtons = () => {
   <button class='btn btn-lg btn-primary m-2'
     id = '` +
         letter +
-        `
+        `'
     onClick="handleGuess('` +
         letter +
         `')"
@@ -51,7 +51,36 @@ const generateButtons = () => {
   document.getElementById('keyboard').innerHTML = buttonHtml;
 };
 
+const guessedWord = () => {
+  wordStatus = answer
+    .split('')
+    .map((letter) => (guessed.indexOf(letter) >= 0 ? letter : ' _ '))
+    .join('');
+
+  console.log(answer);
+
+  console.log(
+    answer
+      .split('')
+      .map((letter) => (guessed.indexOf(letter) >= 0 ? letter : ' _ '))
+  );
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+};
+
+const handleGuess = (chosenLetter) => {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  alert(answer);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+  }
+};
+
 document.getElementById('maxWrong').innerHTML = maxWrong;
 
 randomWord();
 generateButtons();
+guessedWord();
